554eeb334f43771682fa4741d8b855a8625c2550
Dayne Dewar <daynedewar@yahoo.com>
2021-01-03 00:46:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,7 +129,7 @@ end
 def num_points_scored(player)
   game_hash.each do |team, team_info|
     team_info[:players].each do |starting_five|
-     if [:player_name] == player
+     if starting_five[:player_name] == player
        starting_five[:points]
      end
    end
